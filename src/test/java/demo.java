fd7521ea8c4ec571d95099f2af01e482ca2bc761
uswahshamim <uswahshamim4890@gmail.com>
2021-12-02 00:24:55 +0500

    first commit

--- src/test/java/demo.java
+++ src/test/java/demo.java
@@ -1,7 +1,6 @@
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,6 +15,7 @@ import static java.lang.Thread.*;
 public class demo {
 
     WebDriver driver;
+    WebElement signIn;
     public void browserActivity(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\uswa.shamim\\Downloads\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
@@ -25,81 +25,17 @@ public class demo {
         driver.get("http://automationpractice.com/index.php");
 
     }
-
-
-    public void logIn() throws InterruptedException {
-        WebElement signIn;
+    public void signIn()throws InterruptedException{
+        sleep(2000);
         signIn = driver.findElement(By.linkText("Sign in"));
         signIn.click();
-        //driver.findElement(By.xpath("//input[@name='email_create']")).sendKeys("uswashahmim777@gmail.com");
-        //".cssSelector("[id='email_create']")).click();
-
-        //create account(email enter)
-        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
-        driver.findElement(By.cssSelector("[name='email_create']")).sendKeys("uswashahmim778@gmail.com");
-        driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[1]/form/div/div[3]/button/span")).click();
         sleep(2000);
-        //create account(registration form)
-        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
-        driver.findElement(By.xpath("//*[@id='account-creation_form']/div[1]/div[1]/div[2]/label")).click();
-        driver.findElement(By.name("customer_firstname")).sendKeys("uswah");
-        driver.findElement(By.name("customer_lastname")).sendKeys("shamim");
-        driver.findElement(By.name("passwd")).sendKeys("48900uswah");
-        //driver.findElement(By.id("days")).click();
-        Select drpDay = new Select(driver.findElement(By.name("days")));
-        drpDay.selectByValue("17");
-        Select drpMonth = new Select(driver.findElement(By.name("months")));
-        drpMonth.selectByValue("3");
-        Select drpYear = new Select(driver.findElement(By.name("years")));
-        drpYear.selectByValue("1999");
-        driver.findElement(By.id("optin")).click();
-        driver.findElement(By.id("company")).sendKeys("10pearls");
-        driver.findElement(By.id("address1")).sendKeys("PECHS block 6 parsa tower 9th floor");
-        driver.findElement(By.id("address2")).sendKeys("PECHS block 6 parsa tower 8th floor");
-        driver.findElement(By.id("city")).sendKeys("karachi");
-        Select drpState = new Select(driver.findElement(By.name("id_state")));
-        drpState.selectByValue("9");
-        driver.findElement(By.id("postcode")).sendKeys("74600");
-        //Select drpCountry = new Select(driver.findElement(By.name("id_country")));
-        //drpCountry.selectByVisibleText("United State");
-        driver.findElement(By.id("other")).sendKeys("abc");
-        driver.findElement(By.id("phone")).sendKeys("03152914647");
-        driver.findElement(By.id("phone_mobile")).sendKeys("03320374386");
-        driver.findElement(By.id("alias")).sendKeys("PECHS block 6 karachi");
-        driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/form/div[4]/button/span")).click();
+        driver.findElement(By.cssSelector("[name='email']")).sendKeys("uswahshamim777@gmail.com");
         sleep(2000);
-        driver.findElement(By.name("submitAccount")).click();
-        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
-        //String userText=driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).getText();
-        String userText = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).getText();
-
-        // Validate that user has created
-        if (userText.contains("uswahshamim")) {
-            System.out.println("User Verified,Test case Passed");
-        } else {
-            System.out.println("User Verification Failed,Test case Failed");
-        }
-        driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
-        //signout
-        // driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
-        //driver.findElement(By.linkText("Sign out")).click();
-        //driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
-        //signup(enter email and password)
-        //WebElement email;
-        //driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
-        //email = driver.findElement(By.id("email"));
-        //email.sendKeys("uswahshamim772@gmail.com");
-        //driver.findElement(By.id("passwd")).sendKeys("48900uswah");
-        //Thread.sleep(2000);
-        //driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button/span")).click();
-        //driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
-        //forgot password
-        //driver.navigate().back();
-        //driver.findElement(By.linkText("Forgot your password?")).click();
-        //email.sendKeys("uswahshamim770@gmail.com");
-        //driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/form/fieldset/p/button/span")).click();
-        //driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/ul/li/a/span")).click();
-        //}
+        driver.findElement(By.cssSelector("[id='passwd']")).sendKeys("48900uswah");
+        sleep(2000);
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button/span")).click();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
     }
     public void searchItem()throws InterruptedException{
         sleep(2000);
@@ -122,7 +58,6 @@ public class demo {
 
         }
     }
-
     public void fetchAndPrint()throws InterruptedException{
         String fetchPrice=driver.findElement(By.id("our_price_display")).getText();
         System.out.println(fetchPrice);
@@ -154,29 +89,25 @@ public class demo {
         driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
         //1st step of Checkout page Proceed
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/p[2]/a[1]/span")).click();
-        driver.findElement(By.name("messgae")).sendKeys("abc");
+        //driver.findElement(By.name("messgae")).sendKeys("abc");
         driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
-        //2nd step of signin
-        //WebElement email;
-        sleep(2000);
-        driver.findElement(By.id("email")).sendKeys("uswahshamim777@gmail.com");
-        sleep(2000);
-        driver.findElement(By.id("passwd")).sendKeys("48900uswah");
-        sleep(2000);
-        driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button/span")).click();
-        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
-        //3rd step of Agree terms&Conditions
+        //3rd step address
+        driver.findElement(By.name("message")).sendKeys("abc");
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/form/p/button/span")).click();
+        driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+        //4th step of Agree terms&Conditions
         driver.findElement(By.name("cgv")).click();
         driver.findElement(By.xpath("//*[@id='form']/p/button/span")).click();
         driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
 
-        //4th step of Click on Payby Check
-        driver.findElement(By.linkText("Pay by check.")).click();
-        //5th step of order confirmation
+        //5th step of Click on Payby Check
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[3]/div[2]/div/p/a")).click();
+        driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+        //6th step of order confirmation
         driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/form/p/button/span")).click();
         driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
 
-        //Get Text and store it in a astring
+        //Get Text and store it in a string
         String ConfirmationText = driver.findElement(By.xpath("//div[@id='center_column']/p[@class='alert alert-success']")).getText();
         driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
         // Verify that Product is being ordered
@@ -188,13 +119,87 @@ public class demo {
         }
     }
 
+    public void logIn() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+        driver.findElement(By.linkText("Sign out")).click();
+        driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+        //create account(email enter)
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        driver.findElement(By.cssSelector("[name='email_create']")).sendKeys("uswashahmim099@gmail.com");
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[1]/form/div/div[3]/button/span")).click();
+        sleep(2000);
+        //create account(registration form)
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        driver.findElement(By.xpath("//*[@id='account-creation_form']/div[1]/div[1]/div[2]/label")).click();
+        driver.findElement(By.name("customer_firstname")).sendKeys("uswah");
+        driver.findElement(By.name("customer_lastname")).sendKeys("shamim");
+        driver.findElement(By.name("passwd")).sendKeys("48900uswah");
+        //date of birth;
+        Select drpDay = new Select(driver.findElement(By.name("days")));
+        drpDay.selectByValue("17");
+        Select drpMonth = new Select(driver.findElement(By.name("months")));
+        drpMonth.selectByValue("3");
+        Select drpYear = new Select(driver.findElement(By.name("years")));
+        drpYear.selectByValue("1999");
+        //address
+        driver.findElement(By.id("optin")).click();
+        driver.findElement(By.id("company")).sendKeys("10pearls");
+        driver.findElement(By.id("address1")).sendKeys("PECHS block 6 parsa tower 9th floor");
+        driver.findElement(By.id("address2")).sendKeys("PECHS block 6 parsa tower 8th floor");
+        driver.findElement(By.id("city")).sendKeys("karachi");
+        Select drpState = new Select(driver.findElement(By.name("id_state")));
+        drpState.selectByValue("9");
+        driver.findElement(By.id("postcode")).sendKeys("74600");
+        //Select drpCountry = new Select(driver.findElement(By.name("id_country")));
+        //drpCountry.selectByVisibleText("United State");
+        driver.findElement(By.id("other")).sendKeys("abc");
+        driver.findElement(By.id("phone")).sendKeys("03152914647");
+        driver.findElement(By.id("phone_mobile")).sendKeys("03320374386");
+        driver.findElement(By.id("alias")).sendKeys("PECHS block 6 karachi");
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/form/div[4]/button/span")).click();
+        sleep(2000);
+        driver.findElement(By.name("submitAccount")).click();
+        driver.manage().timeouts().implicitlyWait(8000, TimeUnit.MILLISECONDS);
+        //Validation of user creation
+        String userVerification=driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")).getText();
+        if (userVerification.contains("uswahshamim")) {
+            System.out.println("User Verified,Test case Passed");
+        } else {
+            System.out.println("User Verification Failed,Test case Failed");
+        }
+        driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+        driver.quit();
+        //signout
+        //driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+        //Thread.sleep(4000);
+        //driver.findElement(By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[2]/a")).click();
+        //driver.manage().timeouts().implicitlyWait(8000, TimeUnit.MILLISECONDS);
+        //signup(enter email and password)
+        //WebElement email;
+        //driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+        //email = driver.findElement(By.id("email"));
+        //email.sendKeys("uswahshamim772@gmail.com");
+        //driver.findElement(By.id("passwd")).sendKeys("48900uswah");
+        //Thread.sleep(2000);
+        //driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button/span")).click();
+        //driver.manage().timeouts().implicitlyWait(4000, TimeUnit.MILLISECONDS);
+        //forgot password
+        //driver.navigate().back();
+        //driver.findElement(By.linkText("Forgot your password?")).click();
+        //email.sendKeys("uswahshamim770@gmail.com");
+        //driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/form/fieldset/p/button/span")).click();
+        //driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/ul/li/a/span")).click();
+        //}
+    }
 
     public static void main(String[] args) throws InterruptedException {
         demo obj= new demo();
         obj.browserActivity();
         //obj.logIn();
+        obj.signIn();
         obj.searchItem();
         obj.fetchAndPrint();
         obj.addToCart();
+        obj.logIn();
     }
 }
